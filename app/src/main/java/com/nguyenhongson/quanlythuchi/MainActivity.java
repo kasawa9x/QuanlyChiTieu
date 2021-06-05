d4619681c56752a134f96d69bbbfcbd843cd6d4a
Nguyễn Hồng Sơn <53332944+kasawa9x@users.noreply.github.com>
2021-06-05 15:51:52 +0700

    1st

---
+++ app/src/main/java/com/nguyenhongson/quanlythuchi/MainActivity.java
@@ -0,0 +1,111 @@
+package com.nguyenhongson.quanlythuchi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+
+import com.google.android.material.navigation.NavigationView;
+import com.nguyenhongson.quanlythuchi.fragment.ChangepasswordFragment;
+
+import com.nguyenhongson.quanlythuchi.fragment.KhoanChi_Fragment;
+import com.nguyenhongson.quanlythuchi.fragment.KhoanThu_Fragment;
+import com.nguyenhongson.quanlythuchi.fragment.ThongKe_Fragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle toggle;
+
+    FrameLayout frameLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        frameLayout = findViewById(R.id.frame_layout);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        setupDrawerContent(navigationView);
+        ThongKe_Fragment searchFragment = new ThongKe_Fragment();
+        replaceFragment(searchFragment);
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, fragment).commit();
+    }
+
+    private void selectedItemDrawer(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.khoanthu:
+                setTitle("KHOẢN THU");
+                KhoanThu_Fragment khoanThu_fragment = new KhoanThu_Fragment();
+                replaceFragment(khoanThu_fragment);
+                break;
+            case R.id.khoanchi:
+                setTitle("KHOẢN CHI");
+                KhoanChi_Fragment khoanChi_fragment = new KhoanChi_Fragment();
+                replaceFragment(khoanChi_fragment);
+                break;
+            case R.id.thongke:
+                setTitle("THỐNG KÊ");
+                ThongKe_Fragment searchFragment = new ThongKe_Fragment();
+                replaceFragment(searchFragment);
+                break;
+
+
+            case R.id.doimatkhau:
+                setTitle("ĐỔI MẬT KHẨU");
+                ChangepasswordFragment changepasswordFragment = new ChangepasswordFragment();
+                replaceFragment(changepasswordFragment);
+                break;
+            case R.id.logout:
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+                break;
+
+        }
+
+        item.setChecked(true);
+
+        drawerLayout.closeDrawers();
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                selectedItemDrawer(item);
+                return true;
+            }
+        });
+    }
+
+}
